299cd0c0399014731339c6a35621d1e89dea8072
Anish Vadher <anishvadher@anishs-mbp.home>
2017-10-02 17:59:53 +0100

    created game logic

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Up and running!");
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
+
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardOne);
