7a54af6c70af6007778a68aa9496f4e85b34c256
Anish Vadher <anishvadher@anishs-mbp.home>
2017-10-04 16:54:37 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
 
-var cardTwo = cards[1];
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again.");
+	}
+};
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
 
-console.log("user flipped " + cardsInPlay);
+flipCard(0);
+flipCard(1);
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match");
-} else {
-	alert("Sorry, try again");
-}
