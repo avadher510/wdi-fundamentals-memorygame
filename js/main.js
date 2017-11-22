b8c6449bcd2dd2b58d3a38f22c7b7dd307947a72
Anish Vadher <anishvadher@anishs-mbp.home>
2017-11-22 15:34:00 +0000

    reset button added

--- js/main.js
+++ js/main.js
@@ -24,33 +24,53 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
+	/* 
+	This was my original - why does this new method work?
 	this.setAttribute('src', cards[cardId].cardImage);
-	if (cardsInPlay.length === 2) {
+	if (cardsInPlay.length === 2) { */
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+		alert("You found a match!");
 	} else {
-	alert("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
-}
 };
 
 var flipCard = function() {
 	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	checkForMatch();
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 };
 
 var createBoard = function() {
 	for (var i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement('img');
-		cardElement.setAttribute('src', 'images/back.png');
-		cardElement.setAttribute('data-id', 'i');
-		cardElement.addEventListener('click', flipCard);
-		document.getElementById('game-board').appendChild(cardElement);
+			cardElement.setAttribute('src', 'images/back.png');
+			cardElement.setAttribute('data-id', i);
+			cardElement.addEventListener('click', flipCard);
+			document.getElementById('game-board').appendChild(cardElement);
 	};
 };
 
+
+
 createBoard();
+
+document.getElementById("resetgame").onclick = function() {
+	location.reload();
+};
+
+
+
+
+
+
+
+
+
+
+
+
